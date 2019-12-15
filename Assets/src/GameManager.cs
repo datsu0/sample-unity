b5d12bbe9d90fb478fdb65a2b789e31cc6a84118
datsu <atsu34412@gmail.com>
2019-12-15 22:50:29 +0900

    use async/await

---
+++ Assets/src/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Async;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private string word;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject);
+        var audioSource = gameObject.GetComponent<AudioSource>();
+        Destroy(audioSource);
+        var rididbody = gameObject.AddComponent<Rigidbody>();
+        rididbody.useGravity=false;
+        Timer();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(word);
+    }
+
+    async UniTask Timer()
+    {
+        Debug.Log("hogehoge");
+        await UniTask.Delay(3000);
+        Debug.Log(word);
+        Timer();
+    }
+}
+
+
+

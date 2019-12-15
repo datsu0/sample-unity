7019b25a5d9310f99040d72fafaba6086d76ba41
datsu <atsu34412@gmail.com>
2019-12-15 23:05:29 +0900

    create box

---
+++ Assets/src/createrObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createrObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.AddComponent(typeof(RotateSample));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
